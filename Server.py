7e4339f93cef4c13449396055062dba87b23d5be
talmid 1 <talmid1@BENGDOMAIN.LOCAL>
2025-11-06 09:51:23 +0200

    before step 6 managers

--- Server.py
+++ Server.py
@@ -9,18 +9,20 @@ class ChatServer:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket = None
         self.addr = None
-        self.clients = {}
-        self.clientCount = 0
+        self.socket_to_addr = {}
+        self.clients = []
 
     def start(self):
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen()
-        print(f"Server listening on {self.host}:{self.port}")
-        self.client_socket, self.addr = self.server_socket.accept()
-        print("Connection from", self.addr)
-        self.clients[self.clientCount] = (client_socket, addr)
-        threading.Thread(target=self.handle_client, args=(client_socket, addr), daemon=True).start()
-        self.clientCount += 1
+        while True:
+            print(f"Server listening on {self.host}:{self.port}")
+            self.client_socket, self.addr = self.server_socket.accept()
+            print("Connection from", self.addr)
+            self.socket_to_addr[self.client_socket] = self.addr
+            self.clients.append(self.client_socket)
+            threading.Thread(target=self.handle_client, args=(self.client_socket, self.addr), daemon=True).start()
+
 
     def handle_client(self, client_socket, addr):
         while True:
@@ -29,12 +31,17 @@ class ChatServer:
                 if not data or data.lower() == 'exit':
                     break
                 print(f"{addr}: {data}")
-                client_socket.send(f"Server received: {data}".encode('utf-8'))
+                self.handle_data(client_socket,data)
             except ConnectionResetError:
                 break
         print(f"Connection closed: {addr}")
         client_socket.close()
         self.clients.remove(client_socket)
+
+    def handle_data(self,client_socket,data):  # send the message from one client to all the other clients
+        for client in self.clients:
+            if client != client_socket:
+                client.sendall(data.encode('utf-8'))
 
 
 if __name__ == "__main__":
